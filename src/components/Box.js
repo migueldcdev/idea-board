6c035029c027791f9e82a07e1f0fb9eeea3cb5a2
miguel <miguel@tonio.dev>
2024-07-30 15:12:13 +0200

    added sort select button 0.2

--- src/components/Box.js
+++ src/components/Box.js
@@ -5,6 +5,7 @@ import { ideasContext } from '../context/ideasContext'
 import Tile from './Tile'
 
 import './Box.css'
+import { parse } from 'uuid'
 
 const Box = () => {
 
@@ -12,28 +13,55 @@ const Box = () => {
 
     const [parsedIdeas, setParsedIdeas] = useState([])
 
-    
+    const [sortByDate, setSortByDate] = useState(true)
+    const [sortedIdeas, setSortedIdeas] = useState([])
 
     useEffect(() => {
         if (ideas) setParsedIdeas(JSON.parse(ideas))
     }, [ideas])
 
+    useEffect(() => {
+
+        if (ideas && sortByDate) setSortedIdeas(sortIdeasByDate(parsedIdeas))
+
+        if (ideas && !sortByDate) setSortedIdeas(sortIdeasByTitle(parsedIdeas))
+
+    }, [parsedIdeas, sortByDate])
+
+    function sortIdeasByDate(ideas) {
+
+        const sortedIdeas = ideas.sort((x, y) => new Date(y.date) - new Date(x.date));
+        return sortedIdeas
+    }
+
+    function sortIdeasByTitle(ideas) {
+        console.log("hii")
+        const sortedIdeas = ideas.sort((x, y) => x.title.localCompare(y.title))
+        return sortedIdeas
+    }
+
     return (
-        <div>            
-            {parsedIdeas.length > 0 ?
+        <div>
+            {sortedIdeas.length > 0 ?
             <div>
                 <div className='flex-start-container'>
                     <div className='select-order'>
                         <label>Sort by: </label>
                         <select>
-                            <option selected>Date</option>
-                            <option>A-Z </option>
+                            <option
+                                selected
+                                onSelect={() => setSortByDate(true)}
+                            >
+                                Date
+                            </option>
+
+                            <option onSelect={() => {setSortByDate(false)}}>A-Z </option>
                         </select>
                     </div>
                 </div>
                 <div className='box hide-scrollbar '>
-                    
-                    {parsedIdeas.map((element) => (
+
+                    {sortedIdeas.map((element) => (
                         <Tile
                             key={element.id}
                             props={element}
