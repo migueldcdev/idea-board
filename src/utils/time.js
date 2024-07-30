fbaa1a1800b2266eabbd0e5cf6c8668d0b9bbdbb
miguel <miguel@tonio.dev>
2024-07-30 10:35:58 +0200

    format doc and remove shadow from tile

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,7 +1,7 @@
 const unixToDate = (unix) => {
 
    const date = new Date(unix) 
-   console.log(date) 
+   
    const day = date.getDate()
    const month = date.getMonth()
    const year = date.getFullYear()
