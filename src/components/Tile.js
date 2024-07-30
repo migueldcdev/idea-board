e377e10584f68b4fb6d15f950e3629bf5f118be8
miguel <miguel@tonio.dev>
2024-07-30 10:35:13 +0200

    added remove idea button

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,15 +1,37 @@
 import { useContext } from 'react'
 
+import { ideasContext } from "../context/ideasContext"
+
 import unixToDate from '../utils/time'
 
 import './Tile.css'
 
 const Tile = ({ props }) => {
 
+    const { ideas, setIdeas } = useContext(ideasContext)
+
+    function deleteIdea() {
+        
+        const parsedIdeas = JSON.parse(ideas)
+
+        const id = props.id
+
+        const index = parsedIdeas.findIndex(item => item.id == id)
+
+        if (index !== -1) parsedIdeas.splice(index, 1)
+
+        setIdeas(JSON.stringify(parsedIdeas))
+    }
+
     return (
         <div className='tile'>
-            <div className='date'>
-                {props.updated ? "Updated " : "Created " + unixToDate(props.date)}
+            <div>
+                <div className='delete-button'>
+                    <button onClick={() => deleteIdea()} >x</button>
+                </div>
+                <div className='date'>
+                    {props.updated ? "Updated " : "Created " + unixToDate(props.date)}
+                </div>
             </div>
             <div className='center-items'>
                 <div>
@@ -17,7 +39,7 @@ const Tile = ({ props }) => {
                         type='text'
                         className='title'
                         placeholder='Title'
-                        autoFocus                        
+                        autoFocus
                     />
                 </div>
                 <textarea
