e8cde333741cf2ecdfb2621aa5457773f583a7b8
miguel <miguel@tonio.dev>
2024-07-30 13:35:08 +0200

    center text outside grid box

--- src/components/Box.js
+++ src/components/Box.js
@@ -8,32 +8,39 @@ import './Box.css'
 
 const Box = () => {
 
-    const {ideas} = useContext(ideasContext)
+    const { ideas } = useContext(ideasContext)
 
     const [parsedIdeas, setParsedIdeas] = useState([])
-    
+
     useEffect(() => {
-        if (ideas) setParsedIdeas(JSON.parse(ideas))    
+        if (ideas) setParsedIdeas(JSON.parse(ideas))
     }, [ideas])
 
-    return(
-        
-        <div className='box hide-scrollbar '>
-            {parsedIdeas.length > 0 ?               
-               
-               parsedIdeas.map((element, index )=> (
-                <Tile 
-                    key={index} 
-                    props={element}
-                /> 
-               ))
-            :
-            <p className='no-ideas'>
-                You haven't added any ideas yet.
-            </p>            
-            }       
+    return (
+        <div>
+
+            {parsedIdeas.length > 0 ?
+                <div className='box hide-scrollbar '>
+                    {parsedIdeas.map((element, index) => (
+                        <Tile
+                            key={index}
+                            props={element}
+                        />
+                    ))}
+                </div>
+                :
+                <div>
+                    <p className='no-ideas'>
+                        You haven't added any ideas yet.
+                    </p>
+                </div>
+            }
         </div>
     )
 }
 
 export default Box
+/*
+
+
+*/
