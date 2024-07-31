bfff795bb39ff30a7a04b6781c112353add1675f
miguel <miguel@tonio.dev>
2024-07-31 09:25:12 +0200

    commented sort functionality mobile error

--- src/components/Box.js
+++ src/components/Box.js
@@ -5,7 +5,7 @@ import { ideasContext } from '../context/ideasContext'
 import Tile from './Tile'
 
 import './Box.css'
-import { parse } from 'uuid'
+
 
 const Box = () => {
 
@@ -13,43 +13,42 @@ const Box = () => {
 
     const [parsedIdeas, setParsedIdeas] = useState([])
 
-    const [sortByDate, setSortByDate] = useState(true)
-    const [sortedIdeas, setSortedIdeas] = useState([])
+    // const [sortByDate, setSortByDate] = useState(true)
+    // const [sortedIdeas, setSortedIdeas] = useState([])
 
     useEffect(() => {
         if (ideas) setParsedIdeas(JSON.parse(ideas))
     }, [ideas])
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        if (ideas && sortByDate) setSortedIdeas(sortIdeasByDate(parsedIdeas))
+    //     if (ideas && sortByDate) setSortedIdeas(sortIdeasByDate(parsedIdeas))
 
-        if (ideas && !sortByDate) setSortedIdeas(sortIdeasByTitle(parsedIdeas))
+    //     if (ideas && !sortByDate) setSortedIdeas(sortIdeasByTitle(parsedIdeas))
 
-    }, [parsedIdeas, sortByDate])
+    // }, [parsedIdeas])
 
-    function sortIdeasByDate(ideas) {
+    // function sortIdeasByDate(ideas) {
 
-        const sortedIdeas = ideas.sort((x, y) => new Date(y.date) - new Date(x.date));
-        return sortedIdeas
-    }
+    //     const sortedIdeas = ideas.sort((x, y) => y.date - x.date);
+    //     return sortedIdeas
+    // }
 
-    function sortIdeasByTitle(ideas) {
-        console.log("hii")
-        const sortedIdeas = ideas.sort((x, y) => x.title.localCompare(y.title))
-        return sortedIdeas
-    }
+    // function sortIdeasByTitle(ideas) {
+    //     console.log("hii")
+    //     const sortedIdeas = ideas.sort((x, y) => x.title.localCompare(y.title))
+    //     return sortedIdeas
+    // }
 
     return (
         <div>
-            {sortedIdeas.length > 0 ?
+            {parsedIdeas.length > 0 ?
             <div>
                 <div className='flex-start-container'>
-                    <div className='select-order'>
+                    {/* <div className='select-order'>
                         <label>Sort by: </label>
                         <select>
-                            <option
-                                selected
+                            <option                               
                                 onSelect={() => setSortByDate(true)}
                             >
                                 Date
@@ -57,11 +56,11 @@ const Box = () => {
 
                             <option onSelect={() => {setSortByDate(false)}}>A-Z </option>
                         </select>
-                    </div>
+                    </div> */}
                 </div>
                 <div className='box hide-scrollbar '>
 
-                    {sortedIdeas.map((element) => (
+                    {parsedIdeas.map((element) => (
                         <Tile
                             key={element.id}
                             props={element}
